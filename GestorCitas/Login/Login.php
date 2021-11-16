44cc37a940db8e416167ff4866ee405c9af40f9b
Juan David <david190398@gmail.com>
2021-11-16 00:43:57 -0600

    Primera version de la funcion de logeo

--- GestorCitas/Login/Login.php
+++ GestorCitas/Login/Login.php
@@ -14,16 +14,16 @@
             <h1 id="titulo">Gestor de citas Softville</h1>	  
 		</header>
 		<main>
-			<form id="formulario-login" method="post">
+			<form id="formulario-login" name="formulario-login" method="post" action="">
 				<h2 id="titulo-formulario">Iniciar Sesión</h2>
                 <fieldset id="area-cedula">
                     <label>Cédula</label>
-				    <input type="number" id="cedula" placeholder="116540419 " required/>
+				    <input type="number" id="cedula" name="cedula" placeholder="116540419 " required/>
                 </fieldset>
 
                 <fieldset id="area-contrasena">
                     <label>Contraseña</label>
-				    <input type="password" id="contrasena" placeholder="*********" required/>
+				    <input type="password" id="contrasena" name="contrasena" placeholder="*********" required/>
                 </fieldset>
 
 				<label id="mensaje-error" class="error">
@@ -31,7 +31,7 @@
 				</label>
 
 				<div id="contenedor-boton">
-                	<button type="button" id="ingresar">Ingresar</button>
+                	<button type="submit" id="ingresar">Ingresar</button>
 				</div>
                 <button type="submit" id="submit-escondido" class="hidden-submit" hidden>validar entradas</button>
             </form>
@@ -39,3 +39,20 @@
 
 	</body>
 </html>
+
+<?php
+	if(isset($_POST['cedula']) && isset($_POST['contrasena'])){
+		include 'usuario-class.php';
+		$usuario = new Usuario();
+		$cedula = $_POST["cedula"];
+		/*Aqui hay que trabajar con la clave para evitar insercion de codigo */
+    $clave = $_POST["contrasena"];
+		$_POST["cedula"]= array();
+		$_POST["contrasena"]= array();
+		if($usuario->permisoIngresar($cedula,$clave)){
+			echo '<script language="javascript">alert("Login exitoso, hay que cambiar esta retroalimentación");</script>';
+		}else{
+			echo '<script language="javascript">alert("Logeo incorrecto");</script>';
+		}
+	} 	
+?>
