85118e697e08f6c6b93c55e1ebece2bbb4f0b170
sebastianVUCR <sebastian.vargas.99@gmail.com>
2021-11-16 20:14:00 -0600

    navbar

---
+++ GestorCitas/Compartido/navbar.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="colores.css"/>
+<link rel="stylesheet" href="navbar.css"/>
+
+<nav class="navbar navbar-expand-lg" id="navbar-citas">
+  <button id="boton-menu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <img id="icono-menu" src="icono-menu.jpg" alt="icono menu">
+  </button>
+  
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <li id="li-link-inicio"  class="nav-item active">
+        <a id="link-inicio" class="a-citas nav-link" href="#">Softville</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="a-citas nav-link" href="#">Citas</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Reportes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Procedimientos</a>
+      </li>
+    </ul>
+    <!--
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  -->
+  </div>
+</nav>
